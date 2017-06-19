093ff44e56d367889044b8f0c68e6984d1e88dc3
kevinandres126 <kevinandres.126@gmail.com>
2017-06-19 00:47:05 -0600

    controllers

---
+++ resources/views/User.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Clientes</a>
+                            <a class="navbar-brand" href="#">Productos</a>
+                            <a class="navbar-brand" href="#">Inventario</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Examen 2</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -81,8 +81,7 @@
               <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                   <div class="panel">
-                    @component('components.who')
-                    @endcomponent
+                   <p><strong>EXAMEN 2</strong></p>
                   </div>
                 </div>
               </div>
