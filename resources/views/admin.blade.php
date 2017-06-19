d53ad6884d3b97b7465a06ff65f8d079cb6aa541
kevinandres126 <kevinandres.126@gmail.com>
2017-06-19 01:32:22 -0600

    full

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,10 +5,23 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">ADMIN Dashboard</div>
-
-                <div class="panel-body">
-
+                    <nav class="navbar navbar-default">
+                        <div class="container-fluid">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="{{route('clientes.index')}}">Clientes</a>
+                                <a class="navbar-brand" href="{{route('productos.index')}}">Productos</a>
+                                <a class="navbar-brand" href="{{route('inventario.index')}}">Inventario</a>
+                            </div>
+                        </div>
+                    </nav>
+                    @yield('subcontent')
                 </div>
             </div>
         </div>
